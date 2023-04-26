20f5e5ec957149ddd66cbc14f05b2d571069bf42
Unknown <103126971+HE110W0R19@users.noreply.github.com>
2023-04-26 16:19:05 +0300

    Push new project in HW 5 (Mail message sender)
    Push new project in HW 5 (Mail message sender)

---
+++ HW 5/MailMessageSender/MailMessageSender/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace MailMessageSender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ForTextBox = new System.Windows.Forms.TextBox();
+            this.SubjectTextBox = new System.Windows.Forms.TextBox();
+            this.FromTextBox = new System.Windows.Forms.TextBox();
+            this.MessageRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SendMessageButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.InputMailPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "For:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Subject:";
+            // 
+            // ForTextBox
+            // 
+            this.ForTextBox.Location = new System.Drawing.Point(97, 6);
+            this.ForTextBox.Name = "ForTextBox";
+            this.ForTextBox.Size = new System.Drawing.Size(328, 22);
+            this.ForTextBox.TabIndex = 3;
+            // 
+            // SubjectTextBox
+            // 
+            this.SubjectTextBox.Location = new System.Drawing.Point(97, 34);
+            this.SubjectTextBox.Name = "SubjectTextBox";
+            this.SubjectTextBox.Size = new System.Drawing.Size(328, 22);
+            this.SubjectTextBox.TabIndex = 4;
+            // 
+            // FromTextBox
+            // 
+            this.FromTextBox.Location = new System.Drawing.Point(97, 62);
+            this.FromTextBox.Name = "FromTextBox";
+            this.FromTextBox.Size = new System.Drawing.Size(328, 22);
+            this.FromTextBox.TabIndex = 5;
+            // 
+            // MessageRichTextBox
+            // 
+            this.MessageRichTextBox.Location = new System.Drawing.Point(12, 153);
+            this.MessageRichTextBox.Name = "MessageRichTextBox";
+            this.MessageRichTextBox.Size = new System.Drawing.Size(486, 250);
+            this.MessageRichTextBox.TabIndex = 6;
+            this.MessageRichTextBox.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 134);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Message:";
+            // 
+            // SendMessageButton
+            // 
+            this.SendMessageButton.Location = new System.Drawing.Point(12, 409);
+            this.SendMessageButton.Name = "SendMessageButton";
+            this.SendMessageButton.Size = new System.Drawing.Size(142, 36);
+            this.SendMessageButton.TabIndex = 8;
+            this.SendMessageButton.Text = "Send";
+            this.SendMessageButton.UseVisualStyleBackColor = true;
+            this.SendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 93);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password:";
+            // 
+            // InputMailPassword
+            // 
+            this.InputMailPassword.Location = new System.Drawing.Point(97, 90);
+            this.InputMailPassword.Name = "InputMailPassword";
+            this.InputMailPassword.Size = new System.Drawing.Size(328, 22);
+            this.InputMailPassword.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 459);
+            this.Controls.Add(this.InputMailPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.SendMessageButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.MessageRichTextBox);
+            this.Controls.Add(this.FromTextBox);
+            this.Controls.Add(this.SubjectTextBox);
+            this.Controls.Add(this.ForTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ForTextBox;
+        private System.Windows.Forms.TextBox SubjectTextBox;
+        private System.Windows.Forms.TextBox FromTextBox;
+        private System.Windows.Forms.RichTextBox MessageRichTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button SendMessageButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox InputMailPassword;
+    }
+}
+
---
+++ HW 5/MailMessageSender/MailMessageSender/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailMessageSender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMessageButton_Click(object sender, EventArgs e)
+        {
+            MailAddress FromAddress = new MailAddress(FromTextBox.Text);
+            MailAddress ToAddress = new MailAddress(ForTextBox.Text);
+            MailMessage Message = new MailMessage(FromAddress, ToAddress);
+            Message.Subject = SubjectTextBox.Text;
+            Message.Body = MessageRichTextBox.Text;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.Port = 587;
+            smtpClient.EnableSsl = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = false;
+
+            smtpClient.Credentials = new NetworkCredential(FromAddress.Address, InputMailPassword.Text);
+            smtpClient.Send(Message);
+        }
+    }
+}
